022d0ca3fadeb1f246625e6709320d0e5a5c9d16
Khan Vuthy <khan.vuthy@somatrading.local>
2019-10-31 10:03:33 +0700

    Add permission to module

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'mail', 'hr'],
+    'depends': ['base', 'mail', 'hr','helpdesk'],
 
     # always loaded
     'data': [
