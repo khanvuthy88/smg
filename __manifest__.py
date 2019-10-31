105b5409eee9dacd1a1370629e4ef11dbe72e584
khanvuthy88@gmail.com <khan.vuthy@somatrading.local>
2019-10-31 10:13:36 +0700

    Upgrade security and enable security

--- __manifest__.py
+++ __manifest__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'security/user_group.xml',
         'views/views.xml',
         'views/helpdesk.xml',
